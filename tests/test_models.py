eaed1cb5d127304d32670e1076fcbf54b098ab64
yro7 <yronusa2000@gmail.com>
2026-06-10 14:34:29 +0200

    test: add model tests

---
+++ tests/test_models.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+
+def test_adaptive_cnn_in_channels() -> None:
+    """Verifies that the AdaptiveCNN model accepts inputs with modified channel sizes."""
+    pass
